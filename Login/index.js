cbf10ec58d962f8f2a0ca18cb7a112ddcef484b1
Hemanth Varma Kalidindi <120732381+gs-hvkalidindi@users.noreply.github.com>
2023-09-10 12:58:58 +0530

    Update index.js

--- Login/index.js
+++ Login/index.js
@@ -3,15 +3,7 @@ let login = document.querySelector(".login");
 let slider = document.querySelector(".slider");
 let formSection = document.querySelector(".form-section");
 
-<!-- Gainsight PX Tag-->
-  <script type="text/javascript">
-  (function(n,t,a,e,co){var i="aptrinsic";n[i]=n[i]||function(){
-      (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
-    var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
-    var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-  })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-HDDQKPNBMNFE-1");
-</script>
-<!-- Gainsight PX Tag-->
+
 
 
 signup.addEventListener("click", () => {
