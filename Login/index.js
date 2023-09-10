ba98d35a663cd45bbf26ab6250dbfce719c33a6c
Hemanth Varma Kalidindi <120732381+gs-hvkalidindi@users.noreply.github.com>
2023-09-10 13:00:38 +0530

    Update index.js

--- Login/index.js
+++ Login/index.js
@@ -3,9 +3,6 @@ let login = document.querySelector(".login");
 let slider = document.querySelector(".slider");
 let formSection = document.querySelector(".form-section");
 
-
-
-
 signup.addEventListener("click", () => {
 	slider.classList.add("moveslider");
 	formSection.classList.add("form-section-move");
