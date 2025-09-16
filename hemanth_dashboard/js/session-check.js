8a36a8396dc577bc57fb3e0a547b5cc364bddfe6
Hemanth Varma Kalidindi <120732381+gs-hvkalidindi@users.noreply.github.com>
2025-09-16 13:55:25 +0530

    Update session-check.js

--- hemanth_dashboard/js/session-check.js
+++ hemanth_dashboard/js/session-check.js
@@ -1,6 +1,13 @@
 // session-check.js
 document.addEventListener("DOMContentLoaded", function () {
-    if (sessionStorage.getItem("loggedIn") !== "true") {
+    if (localStorage.getItem("loggedIn") !== "true") {
+        window.location.href = "signin.html";
+    }
+});
+
+// Listen for logout in other tabs
+window.addEventListener("storage", function(event) {
+    if (event.key === "loggedIn" && event.newValue !== "true") {
         window.location.href = "signin.html";
     }
 });
