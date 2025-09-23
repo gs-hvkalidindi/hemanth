3495e1e7ef664bbd61e3b2e6df8a0fae9bb57b64
Hemanth Varma Kalidindi <hvkalidindi@gainsight.com>
2025-09-23 16:13:05 +0530

    added price attribute to users.js

--- hemanth_dashboard/js/users.js
+++ hemanth_dashboard/js/users.js
@@ -6,20 +6,23 @@ const users = [
     password: "PX@12",
     firstName: "Hemanth",
     lastName: "demo",
-    plan: "gold"
+    plan: "gold",
+    price: "999"
   },
   {
     email: "gstesting_1@outlook.com",
     password: "Guest@123",
     firstName: "GS",
     lastName: "Outlook",
-    plan: "silver"
+    plan: "silver",
+    price: "699"
   },
   {
     email: "hvkalidindi@gainsight.com",
     password: "Guest@123",
     firstName: "Hemanth",
     lastName: "PX",
-    plan: "gold"
+    plan: "gold",
+    price: "999"
   }
 ];
